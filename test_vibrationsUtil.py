95e7f4d7fb155b1e00de6da54649d047448487a8
Bruno Eduardo <bruno97br@gmail.com>
2020-11-19 21:21:33 -0300

    branch for future test rotines

---
+++ test_vibrationsUtil.py
@@ -0,0 +1,40 @@
+from telegram import *
+from generalUtil import *
+
+def test_telegram_bot_sendtext():
+    assert telegram_bot_sendtext("Testing a message") != None
+
+def test_confusion_matrix():
+    #TODO: implemet
+    pass
+def test_csv2array():
+    #TODO: implemet
+    pass
+def test_plotD():
+    #TODO: implemet
+    pass
+def test_debug():
+    debugReturn = debug("test", "test_parameter", False)
+    assert debugReturn == None
+def test_confusionMatrixPrint():
+    #TODO: implemet
+    pass
+def test_getBatch():
+    #TODO: implemet
+    pass
+def test_prepareBatches():
+    #TODO: implemet
+    pass
+def test_generateScratch():
+    #TODO: implemet
+    pass
+def test_setLayersAndCompile():
+    #TODO: implemet
+    pass
+def test_saveModel():
+    #TODO: implemet
+    pass
+def test_main():
+    #TODO: implemet
+    pass
+
